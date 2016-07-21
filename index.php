a56da062995fb55c402783fefdd27a5770cfac47
SRAUFactory <srau.factory@gmail.com>
2016-07-21 23:25:44 +0900

    ダウンロードの不具合修正

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 require('classes/WDEController.php');
 $controller = new WDEController();
 if (isset($_POST["download"])) {
-    $result = $controller->download();
+    $response = $controller->download();
+    foreach ($response["header"] as $header) {
+        header($header);
+    }
+    $result = $response["body"];
 } elseif (isset($_POST["addRow"]) || isset($_POST["addCol"])) {
     $result = $controller->addRowCol();
 } elseif (isset($_FILES["uploadFile"])) {
