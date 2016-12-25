0fd4a6e4a8068d2e517bef0dd505e6cc608d9850
SRAU Factory <srau.factory@araiyuuki-no-MacBook-Air.local>
2016-12-25 20:36:28 +0900

    ファイルアップロード処理追加
    #15

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-angular.module('WebDataEditor', [])
-       .controller('EditorController', function($scope) {
+angular.module('WebDataEditor', []).controller('EditorController', function($scope) {
            $scope.upload = function() {
-               console.log("WebDataEditor.upload");
+               console.log("WebDataEditor.upload1234");
+               console.log($scope.file);
            };
 
            $scope.addRowCol = function() {
@@ -10,4 +10,17 @@ angular.module('WebDataEditor', [])
            $scope.download = function() {
                console.log("WebDataEditor.download");
            }
+       }).directive('fileModel',function($parse) {
+           return{
+               restrict: 'A',
+               link: function(scope,element,attrs) {
+                   var model = $parse(attrs.fileModel);
+                   element.bind('change',function() {
+                       scope.$apply(function() {
+
+                           model.assign(scope,element[0].files[0]);
+                       });
+                   });
+               }
+           };
        });
