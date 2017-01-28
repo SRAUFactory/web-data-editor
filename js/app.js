d691ccfda1bb152b69c689a25b9114260aaf91a9
SRAUFactory <srau.factory@gmail.com>
2017-01-28 21:20:24 +0900

    保存用のModalを追加
    #26

--- js/app.js
+++ js/app.js
@@ -14,24 +14,43 @@ angular.module('WebDataEditor', ['ui.bootstrap']).controller('EditorController',
            $scope.lfCode = "lf";
            $scope.list = [];
 
-           $scope.open = function() {
+           $scope.openFile = function() {
+               $scope.openFileModal(true);
+           };
+
+           $scope.saveFile = function() {
+               $scope.openFileModal(false);
+           }
+
+           $scope.openFileModal = function(isNew) {
                $scope.file = null;
                var modalInstance = $uibModal.open({
                    templateUrl: "file-modal",
                    controller: 'ModalCtrl',
                    scope: $scope,
+                   resolve: {
+                       isNew: isNew 
+                   }
                });
+
                modalInstance.result.then(function() {
-                   if (modalInstance.result.$$state.value) {
+                   if (!modalInstance.result.$$state.value) {
+                       return;
+                   }
+                   if (typeof modalInstance.result.$$state.value.file !== "undefined") {
                        $scope.getList(modalInstance.result.$$state.value);
+                   }
+                   if (typeof modalInstance.result.$$state.value.fileName !== "undefined") {
+                       $scope.download(modalInstance.result.$$state.value);
                    }
                }, function() {
                });
            };
-           $scope.download = function() {
+
+           $scope.download = function(result) {
                let temp = [];
-               let separator = $scope.columnSeparetor[$scope.fileType];
-               let lfCode = $scope.lfCodeList[$scope.lfCode];
+               let separator = $scope.columnSeparetor[result.fileType];
+               let lfCode = $scope.lfCodeList[result.lfCode];
                $scope.list.forEach(function(cols, index) {
                    temp[index] = cols.join(separator);
                });
@@ -39,7 +58,7 @@ angular.module('WebDataEditor', ['ui.bootstrap']).controller('EditorController',
                let bom = new Uint8Array([0xEF, 0xBB, 0xBF]);
                let blob = new Blob([bom, contents], { type: "text/plain" });
                let link = document.getElementById("download");
-               link.download = $scope.file.name;
+               link.download = result.fileName;
                link.href = window.URL.createObjectURL(blob);
            };
 
@@ -85,11 +104,14 @@ angular.module('WebDataEditor', ['ui.bootstrap']).controller('EditorController',
                    }); 
                };
                $scope.file = info.file;
+               $scope.fileName = info.file.name;
                $scope.fileType = info.fileType;
                $scope.lfCode = info.lfCode;
                reader.readAsText(info.file);
            }
-       }]).controller('ModalCtrl', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance) {
+       }]).controller('ModalCtrl', ['$scope', '$uibModalInstance', 'isNew', function($scope, $uibModalInstance, isNew) {
+           $scope.isNew = isNew;
+
            $scope.$watch("file", function(file) {
                if(!file || (!file.type.match('text/csv') && !file.type.match('text/tab-separated-values'))) {
                    return;
@@ -104,6 +126,15 @@ angular.module('WebDataEditor', ['ui.bootstrap']).controller('EditorController',
 
            $scope.close = function() {
                $uibModalInstance.close();
+           };
+
+           $scope.save = function() {
+               let result = {
+                   fileName: $scope.fileName,
+                   lfCode: $scope.lfCode,
+                   fileType: $scope.fileType,
+               };
+               $uibModalInstance.close(result);
            };
        }]).directive('fileModel', function($parse) {
            return{
